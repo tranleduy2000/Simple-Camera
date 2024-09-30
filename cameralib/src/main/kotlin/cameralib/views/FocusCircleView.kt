af6dbb4d8f20848790f23b7545fdcdc4be9206df
duy <duy@mail.com>
2024-09-30 19:39:39 +0700

    Use dynamic size for FocusCircleView.kt

--- cameralib/src/main/kotlin/cameralib/views/FocusCircleView.kt
+++ cameralib/src/main/kotlin/cameralib/views/FocusCircleView.kt
@@ -4,12 +4,13 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.os.Handler
+import android.util.TypedValue
 import android.view.ViewGroup
 import cameralib.extensions.getProperPrimaryColor
 
 class FocusCircleView(context: Context) : ViewGroup(context) {
-    private val CIRCLE_RADIUS = 50f
-    private val CIRCLE_DURATION = 500L
+    private val circleRadius: Float
+    private val animationDuration = 500L
 
     private var mDrawCircle = false
     private var mHandler: Handler
@@ -18,12 +19,16 @@ class FocusCircleView(context: Context) : ViewGroup(context) {
     private var mLastCenterY = 0f
 
     init {
+        val r = context.resources
+        circleRadius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 30.toFloat(), r.displayMetrics)
+        val circleStrokeWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 3.toFloat(), r.displayMetrics)
+
         setWillNotDraw(false)
         mHandler = Handler()
         mPaint = Paint().apply {
             style = Paint.Style.STROKE
             color = context.getProperPrimaryColor()
-            strokeWidth = 2f
+            strokeWidth = circleStrokeWidth
         }
     }
 
@@ -39,7 +44,7 @@ class FocusCircleView(context: Context) : ViewGroup(context) {
         mHandler.removeCallbacksAndMessages(null)
         mHandler.postDelayed({
             toggleCircle(false)
-        }, CIRCLE_DURATION)
+        }, animationDuration)
     }
 
     private fun toggleCircle(show: Boolean) {
@@ -52,7 +57,7 @@ class FocusCircleView(context: Context) : ViewGroup(context) {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         if (mDrawCircle) {
-            canvas.drawCircle(mLastCenterX, mLastCenterY, CIRCLE_RADIUS, mPaint)
+            canvas.drawCircle(mLastCenterX, mLastCenterY, circleRadius, mPaint)
         }
     }
 }
